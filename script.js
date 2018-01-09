4293ac8c704c79a1b502232c9feb2b4fe5b96fe3
Nithin Biliya <nithin.biliya@yahoo.in>
2018-01-09 22:14:21 +0530

    fixed issues #23 and #21 (#24)
    * 1. fixed issue #23 - undo button visible and clickable when showing the game result
    2. removed duplicate checkGameOver function
    
    * fixed issue - #21 - The game closes before you can see the reults of the last turn.

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ initialise();
 
 function initialise()
 {
+	document.getElementById("undo").style.visibility = "visible";
 	isGameOver = false;
 	matrixDefault();
 	drawArena();
@@ -163,17 +164,6 @@ function gameLoop(event)
 	}
 }
 
-function checkGameOver()
-{
-	if(gameOver() == 1 || gameOver() == 2)
-	{
-		isGameOver = true;
-		gameOverScreen(gameOver());
-		clearInterval(gameTimer);
-		setTimeout(initialise, 4000);
-	}
-}
-
 function populateCornerCells(i, j){
 	countMatrix[i][j] -= 2;
 	countMatrix[ i == 8 ? i-1 : i+1 ][j]++;
@@ -260,9 +250,11 @@ function checkGameOver()
 	if(gameOver() == 1 || gameOver() == 2)
 	{
 		isGameOver = true;
-		gameOverScreen(gameOver());
+		document.getElementById("undo").style.visibility = "hidden";
+		drawArena();
+		setTimeout(gameOverScreen.bind(null,gameOver()), 2000);
 		clearInterval(gameTimer);
-		setTimeout(initialise, 4000);
+		setTimeout(initialise, 6000);
 	}
 }
 
