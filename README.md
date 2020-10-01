fb45efc8881b170abf88489f02113315937bbcbb
MalladiChaitanya <chaitanya.kmc@gmail.com>
2020-10-01 23:57:57 +0530

    Added webpack build

--- README.md
+++ README.md
@@ -1,8 +1,2 @@
 # Chain Reaction!
-Online version of the famous mobile game "Chain Reaction"!
-
-### Online!
-The game is now officially online on:
->[ketanhwr.github.io/chain-reaction]
-
-[ketanhwr.github.io/chain-reaction]: <http://ketanhwr.github.io/chain-reaction>
+This is a Advanced version of famous mobile game "Chain Reaction"!
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: './src/javascript/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].bundle.css"
+        }),
+        new HtmlWebpackPlugin({
+            template: 'index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(mp3)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'assets'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+};
